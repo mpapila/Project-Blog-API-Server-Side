84b9e31f1ed6cda96c2ece19541000384d980063
mpapila <mehmetpapila95@gmail.com>
2024-07-29 09:01:09 -0500

    Add compression middleware for response compression - Add helmet middleware for security enhancements - Update package.json to include compression and helmet dependencies.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import indexRouter from './routes/main'
 import cors from 'cors'
+import compression from 'compression'
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -10,8 +12,16 @@ const port = process.env.PORT;
 app.use(express.json())
 app.use(cors())
 
-
+app.use(compression());
 app.use('/', indexRouter)
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            "script-src": ["'self' 'unsafe-inline'", "code.jquery.com", "cdn.jsdelivr.net"],
+        },
+    }),
+);
 
 
 app.listen(port, () => {
